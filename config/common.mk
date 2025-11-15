51f040fad1825d699d8906d224610e2570f9aeb3
aswin7469 <aswinas@pixysos.com>
2025-11-15 20:34:03 +0530

    config: log priv app permissions
    Change-Id: I3363fd90260c7809255a4d501b355f2c51625223
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/common.mk
+++ config/common.mk
@@ -73,7 +73,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
 
-# Enforce privapp-permissions whitelist
+# Log privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
